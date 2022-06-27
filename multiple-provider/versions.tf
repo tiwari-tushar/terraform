d8de13dd8a98e024cdcb67fb5b9c4ec897a996c0
Pandiyan <pandiec@gmail.com>
2022-06-27 21:42:56 +0530

    Adding the provider with Alias and creating VPC on another region

---
+++ multiple-provider/versions.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = "~> 1.2.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0, < 4.20.1"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "3.3.2"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  profile = "default"
+}
+provider "aws" {
+  region = "us-west-1"
+  profile = "default"
+  alias = "aws-west-1"
+}
---
+++ multiple-provider/vpc-east-1.tf
@@ -0,0 +1,6 @@
+resource "aws_vpc" "vpc-us-east-1" {
+  cidr_block = "10.1.0.0/16"
+  tags = {
+    "Name" = "vpc-us-east-1"
+  }
+}
