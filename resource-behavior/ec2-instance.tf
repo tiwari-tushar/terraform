8e320ee2afc416bcb4c07bb4a549e1f44d3d4803
Pandiyan <pandiec@gmail.com>
2022-06-28 09:47:55 +0530

    Changing the availability zone

--- resource-behavior/ec2-instance.tf
+++ resource-behavior/ec2-instance.tf
@@ -4,8 +4,8 @@
 resource "aws_instance" "my-ec2-vm" {
   ami               = "ami-047a51fa27710816e"
   instance_type     = "t2.micro"
-  availability_zone = "us-east-1a"
-  #availability_zone = "us-east-1b"
+  #availability_zone = "us-east-1a"
+  availability_zone = "us-east-1b"
   tags = {
     "Name" = "web"
     "tag1" = "Update-test-1"    
